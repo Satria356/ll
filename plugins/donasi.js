9c8c87139b271bcbaf0e41141cd51bd3028f5540
Satria356 <memeindo74@gmail.com>
2022-10-12 12:41:32 +0700

    first commit

---
+++ plugins/donasi.js
@@ -0,0 +1,31 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Indosat:* [${global.ppulsa}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+❏────
+`
+
+const templateButtons = [
+    {index: 1, urlButton: {displayText: ' Saweria', url: psaweria}},
+    {index: 2, urlButton: {displayText: ' Instagram', url: sig}},
+    {index: 3, urlButton: {displayText: ' Official Group', url: sgc}},
+    {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
