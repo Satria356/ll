8557feb7a79a57c52f471cde71e45211ee0bd47d
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-10-13 23:58:24 +0700

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, {text, args}) => {
-  if (!args[0]) throw `Use example .simi halo`
+  if (!args[0]) throw `Use example .masa halo`
   let api = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=id`)
   let res = await api.json()
   m.reply(res.success)
