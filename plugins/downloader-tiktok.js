4d683530775e21032a47c7d1881ddebbb284bf68
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-10-13 23:57:41 +0700

    Add files via upload

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,12 +1,13 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
-
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih', m)
+import axios from 'axios'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
-handler.limit = true
-module.exports = handler
+handler.command = /^(tik(tok)?(dl)?)$/i
+
+export default handler
